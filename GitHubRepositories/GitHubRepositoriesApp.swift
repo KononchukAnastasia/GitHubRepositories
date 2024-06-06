bdd65e4db02846e3d524c9af8680837c8d89b2ba
Anastasia Kononchuk <nastyakononchuk@me.com>
2024-06-06 14:33:15 +0700

    Initial Commit

---
+++ GitHubRepositories/GitHubRepositoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubRepositoriesApp.swift
+//  GitHubRepositories
+//
+//  Created by Анастасия Конончук on 06.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHubRepositoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
